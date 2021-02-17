6154c77dd37c2e77c097f1f8cf150275b17615cf
Justus Witmer <justus.witmer@gmail.com>
2021-02-17 11:50:02 -0600

    formatted secondary project to include blur

--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -8,28 +8,29 @@ function PortfolioItem(props) {
     return (
       <div className='project-item'>
         <div className='project-item-title'>
-          <h4 tabIndex='0'>{props.project.title}</h4>
+          <h4>{props.project.title}</h4>
+          <p>{props.project.type}</p>
         </div>
         <div className='project-item-image'>
           <img
             src={props.project.image}
-            width='200px'
+            width='250px'
             className='project-imgElement'
             alt={props.project.description}
-            tabIndex='0'
           />
-          <div className='project-description' tabIndex='0'>
+          <div className='project-description'>
             <div className='project-description-divElement'>
               {props.project.description}
               <img 
                 src={arrow} 
                 alt='arrow to external link'
-                tabIndex='0'
                 onClick={() =>  window.open(props.project.link, '_blank')}
                 className='project-description-link'
               />
             </div>
+            <div className='project-description-bg'><img src={props.project.image}/></div>
           </div>
+          
         </div>
       </div>
     );
--- src/redux/reducers/project.reducer.js
+++ src/redux/reducers/project.reducer.js
@@ -1,24 +1,28 @@
 const project = [
   {
     title: 'Movie Library',
+    type: 'Web App Code',
     link: 'https://github.com/justuswitmer/weekend-movie-sagas',
     image: 'images/Movie-Library.png',
     description: 'A single place to store all your movies and find one when you want to watch something! Clicking on a movie will show details about it.',
   },
   {
     title: 'To Do List',
+    type: 'Web App Code',
     link: 'https://github.com/justuswitmer/weekend-sql-to-do-list',
     image: 'images/To-Do-App.png',
     description: 'Get all of the items you need to do out of your head and on to a virtual to do list. Add a to do, categorize it, and then check it off once you comeplete it.',
   },
   {
     title: 'Feedback Survey',
+    type: 'Web App Code',
     link: 'https://github.com/justuswitmer/witmer-redux-feedback-loop',
     image: 'images/Feedback-Loop.png',
     description: 'A feedback form that allows a user to indicate how well they are feeling, being supported, understanding material, and any comments they might have. ',
   },
   {
     title: 'Pizza Parlor',
+    type: 'Web App Code',
     link: 'https://github.com/justuswitmer/redux-pizza-parlor',
     image: 'images/Pizza-Parlor.png',
     description: 'A system for ordering pizza. The user can select their pizzas, the quantity, and then checkout. They can edit their pizzas and track their order through the entire process.',
